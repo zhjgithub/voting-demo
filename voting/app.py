ac6eeae42de5f5784e61670e8eb84bb2410659f3
pkennedyr <pkennedyr@users.noreply.github.com>
2016-03-18 17:28:33 -0700

    Update app.py

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Cats"
+optionB = "Dogs"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
