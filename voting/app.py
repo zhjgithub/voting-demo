3add22789538f6ba20e11261da6a27ac6416f65b
Ryan Kennedy <ryankennedy@C02NH0DFG3QDs-MacBook-Pro.local>
2016-03-21 18:00:14 -0700

    Changed to Dogs + Cats

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Dogs"
+optionB = "Cats"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
